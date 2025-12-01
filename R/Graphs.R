7d175ac170b762ab50ad4b848494201b925920ca
emilie-hebraud <emiliehebraud@gmail.com>
2025-12-02 00:33:42 +0100

    acp and hcpc added
    Refactor statistical analysis and visualization code for reproduced data, including heatmap, volcano plot, and Bland-Altman analysis, acp, hcpc. Added new libraries and improved data handling.

--- R/Graphs.R
+++ R/Graphs.R
@@ -4,6 +4,8 @@ library(RColorBrewer)
 library(ggplot2)
 library(reshape2)
 library(dplyr)
+library(tidyverse)
+library(Factoshiny)
 
 
 #######################################################
@@ -11,10 +13,11 @@ library(dplyr)
 #######################################################
 
 
-### Import raw counts (reproduced data)
+### Importer les donénes reproduites
 counts <- read.table("count_gene.txt",header = TRUE,sep = "\t",comment.char = "#")
 
-#Rename the columns of the data
+### Pré-traitements
+#Renommer les colonnes
 colnames(counts)[colnames(counts) == "SRR10379726_1_trimmed_mapped.bam"] <- "Ctrl3"
 colnames(counts)[colnames(counts) == "SRR10379723_1_trimmed_mapped.bam"] <- "IP3"
 colnames(counts)[colnames(counts) == "SRR10379722_1_trimmed_mapped.bam"] <- "IP2"
@@ -22,8 +25,6 @@ colnames(counts)[colnames(counts) == "SRR10379724_1_trimmed_mapped.bam"] <- "Ctr
 colnames(counts)[colnames(counts) == "SRR10379725_1_trimmed_mapped.bam"] <- "Ctrl2"
 colnames(counts)[colnames(counts) == "SRR10379721_1_trimmed_mapped.bam"] <- "IP1"
 
-
-#Set gene_id as the name of the rows
 rownames(counts) <- counts[,1]
 
 counts <- counts[,-1]
@@ -49,23 +50,18 @@ dds <- DESeqDataSetFromMatrix(countData = reproduced,
                               colData = colData,
                               design = ~ condition)
 
+### Piepline DESeq2
 dds <- DESeq(dds)
-
-### Apply variance stabilizing transformation
 vsd <- vst(dds, blind = FALSE)
 
-### Extraction of top DEGs
+## Extraction des gènes différentiellement exprimés
 res <- results(dds)
 resOrdered <- res[order(res$padj), ]
 DEGs <- subset(resOrdered, padj < 0.1 & abs(log2FoldChange) > 1)
-
-# Keep only genes in the VST data
 vst_mat <- assay(vsd)[rownames(vsd) %in% rownames(DEGs), ]
-
-### Center and scale per gene (optional but recommended)
 vst_mat_scaled <- t(scale(t(vst_mat)))
 
-### Plot the heatmap ###
+### Heatmap
 annotation_col <- data.frame(Condition = factor(colData$condition)) #aggregation of the controls and persisters (names of the )
 rownames(annotation_col) <- rownames(colData)
 my_colors <- colorRampPalette(c("deepskyblue4", "azure", "darkorange2"))(255)
@@ -82,21 +78,19 @@ pheatmap(vst_mat_scaled,
          main = "")
 
 
-### Vulcano plot ###
+### Vulcano plot
 # Volcano plot variables (from the DESeq analysis)
 log_fc <- res$log2FoldChange       # log fold change
 pval <- res$padj                   # adjusted p-value
 
-# Optionally, remove NAs
 valid_idx <- !is.na(log_fc) & !is.na(pval)
 log_fc <- log_fc[valid_idx]
 pval <- pval[valid_idx]
 
-# Significance thresholds
+# Seuils
 seuil <- 0.1           # adjusted p-value threshold
 logfc_cutoff <- 1      # log2 fold change threshold
 
-# Basic volcano plot
 plot(log_fc, -log10(pval),
      pch = 16,
      xlab = "log2 Fold Change",
@@ -104,14 +98,9 @@ plot(log_fc, -log10(pval),
      main = "",
      col = "lightgray")
 
-# Add effect annotation
 mtext("Effect: condition")
-
-# Add significance lines
 abline(h = -log10(seuil), v = c(-logfc_cutoff, logfc_cutoff),
        lwd = 2, col = "orange")
-
-# Highlight significant DEGs
 sig_idx <- which(pval < seuil & abs(log_fc) > logfc_cutoff)
 points(log_fc[sig_idx], -log10(pval[sig_idx]), pch = 16)
 # Optional grid
@@ -242,37 +231,34 @@ ggplot(df_long, aes(x = Article, y = Reproduced, color = Condition)) +
 
 ### graph Bland-Altman MA (Reproductibilité)
 
-#Différence entre log2FoldChange entre les donénes de L'Article et celles Reproduites
+### Analyse Bland-Altman
+## Préparation des données
+#Récupération des log2FoldChange des données publiées et reproduites
 prep_Degs_article <- original_filtered[,c("log2FoldChange","padj")]
 prep_DEGs_article_2 <- subset(prep_Degs_article,padj < 0.1 & abs(log2FoldChange) > 1)
 DEGs_article <- data.frame(Gene= rownames(prep_DEGs_article_2), log2FoldChange = prep_DEGs_article_2$log2FoldChange)
 #Retirer la colonne Gene et la convertir en rownames
 rownames(DEGs_article) <- DEGs_article$Gene
 DEGs_article$Gene <- NULL
-
 DEGs_reproduced <- as.data.frame(DEGs)["log2FoldChange"] #conversion d'un objet  “DESeqResults” en data frame
-length(rownames(original_filtered))
+#length(rownames(original_filtered))
 #1988
-length(rownames(prep_DEGs_article_2))
+#length(rownames(prep_DEGs_article_2))
 #827
-length(rownames(DEGs_article))
+#length(rownames(DEGs_article))
 #827
-length(rownames(DEGs_reproduced))
+#length(rownames(DEGs_reproduced))
 #1174
-DEGs_merged <- merge(DEGs_article, DEGs_reproduced, by = "row.names")
 
-#set rownames with gene names
+DEGs_merged <- merge(DEGs_article, DEGs_reproduced, by = "row.names")
 rownames(DEGs_merged) <- DEGs_merged$Row.names
 DEGs_merged$Row.names <- NULL
-
-# Identify columns by suffix
 degs_article_cols <- grep("\\.x$", names(DEGs_merged), value = TRUE)
-degs_reproduced_cols <- gsub("\\.x$", ".y",degs_article_cols)  # replace .x with .y to find matching names
-
+degs_reproduced_cols <- gsub("\\.x$", ".y",degs_article_cols)
 
 df_long_2 <- do.call(rbind, lapply(seq_along(degs_article_cols), function(i) {
   data.frame(
-    Gene = rownames(DEGs_merged),                    
+    Gene = rownames(DEGs_merged),
     Condition = gsub("\\.x$", "", degs_article_cols[i]),
     Article = DEGs_merged[[degs_article_cols[i]]],
     Reproduced = DEGs_merged[[degs_reproduced_cols[i]]],
@@ -280,21 +266,95 @@ df_long_2 <- do.call(rbind, lapply(seq_along(degs_article_cols), function(i) {
   )
 }))
 
-
-
 df_ba_2 <- data.frame(
   Mean = (df_long_2$Article + df_long_2$Reproduced) / 2,
   Diff = df_long_2$Reproduced - df_long_2$Article,
   Condition = df_long_2$Condition
 )
 
-ggplot(df_ba_2, aes(x = Mean, y = Diff, color = Condition)) +
-  geom_point(alpha = 0.5) +
+df_ba <- data.frame(
+  Mean = (df_long_2$Article + df_long_2$Reproduced) / 2,
+  Diff = df_long_2$Reproduced - df_long_2$Article,
+  Condition = df_long_2$Condition
+)
+## Calcul des différences entre les logfold2change des données de l'Article et Reproduites
+mean_diff <- mean(df_ba$Diff, na.rm = TRUE)
+sd_diff   <- sd(df_ba$Diff, na.rm = TRUE)
+LI <- mean_diff - 1.96 * sd_diff
+LS <- mean_diff + 1.96 * sd_diff
+df_ba$outlier_BA <- df_ba$Diff < LI | df_ba$Diff > LS
+genes_significatifs_BA <- df_ba[df_ba$outlier_BA, ]
+
+##Graphique Bland-Altman
+df_ba$Gene <- rownames(DEGs_merged)
+ggplot(df_ba, aes(x = Mean, y = Diff)) +
+  geom_point(aes(color = outlier_BA), alpha = 0.6) +
+  scale_color_manual(values = c("grey60", "red")) +
+  geom_text_repel(
+    data = df_ba[df_ba$outlier_BA == TRUE, ],
+    aes(label = Gene),
+    size = 3
+  ) +
   geom_hline(yintercept = 0, color = "darkgreen", linetype = "dashed") +
-  geom_hline(yintercept = mean(df_ba_2$Diff), color = "red") +
-  theme_minimal() +
+  geom_hline(yintercept = mean(df_ba$Diff), color = "red")+
+  geom_hline(yintercept = LI, color = "blue", linetype = "dotted") +
+  geom_hline(yintercept = LS, color = "blue", linetype = "dotted") +
+  theme_minimal()+
   labs(
-    x = "Mean expression (Article + Reproduced)/2",
-    y = "Difference (Reproduced − Article)"
+    x = "Expression moyenne (Article + Reproduction)/2",
+    y = "Différence (Reproduction − Article)",
+    color = "Significativité"
   )
 
+
+
+### ACP et HCPC
+
+select1 <- ((original_filtered$padj < 0.1) & (abs(original_filtered$log2FoldChange)>=1))
+class(select1)
+sum(select1)
+
+genePositive_article = original_filtered[select1,]
+genePositive_article <- original_filtered[,c(2:7)]
+
+## Données de l'Article
+x = as.matrix(genePositive_article)
+dta = data.frame(t(x))
+res.PCA<-PCA(dta,graph=TRUE)
+res.HCPC <- HCPC(res.PCA, graph=FALSE)
+#plot.HCPC(res.HCPC,choice='tree',title='Arbre hiérarchique')
+plot.HCPC(res.HCPC,choice='map',draw.tree=FALSE,title='')
+#plot.HCPC(res.HCPC,choice='3D.map',ind.names=FALSE,centers.plot=FALSE,angle=60,title='Arbre hiérarchique sur le plan factoriel')
+
+
+desc_cluster2 = res.HCPC$desc.var$quanti[[2]]
+desc_cluster3 = res.HCPC$desc.var$quanti[[3]]
+
+## Données reproduites
+genePositive_reproduced = counts_filtered[select1,]
+head(genePositive_reproduced)
+genePositive_reproduced <- counts_filtered[,c(4:9)]
+
+x_2 = as.matrix(genePositive_reproduced)
+dta_2 = data.frame(t(x_2))
+res.PCA_2<-PCA(dta_2,graph=TRUE)
+res.HCPC_2 <- HCPC(res.PCA_2, graph=FALSE)
+#plot.HCPC(res.HCPC_2,choice='tree',title='Arbre hiérarchique')
+plot.HCPC(res.HCPC_2,choice='map',draw.tree=FALSE,title='')
+#plot.HCPC(res.HCPC_2,choice='3D.map',ind.names=FALSE,centers.plot=FALSE,angle=60,title='Arbre hiérarchique sur le plan factoriel')
+# Distribution in clusters
+clusters_2 = res.HCPC_2$data.clust$clust
+#length(dta$c.phenotype.lignee.)
+#length(clusters)
+
+# Association gènes clusters  (focus on cluster 2 : Perissters)
+desc_cluster2_2 = res.HCPC_2$desc.var$quanti[[2]]
+head(desc_cluster2_2)
+negative_associations_2_2 = rownames(desc_cluster2_2)[desc_cluster2_2[,1]<0]
+positive_associations_2_2 = rownames(desc_cluster2_2)[desc_cluster2_2[,1]>0]
+
+# Association gènes clusters (focus on cluster 3 : Contrôles)
+desc_cluster3_2 = res.HCPC_2$desc.var$quanti[[3]]
+#head(desc_cluster3_2)
+negative_associations_3_2 = rownames(desc_cluster3_2)[desc_cluster3_2[,1]<0]
+positive_associations_3_2 = rownames(desc_cluster3_2)[desc_cluster3_2[,1]>0]
